96975f3b8cae52bad2a17c53db88bdf3e26e3bea
jason <jason100122@gmail.com>
2024-03-16 19:56:23 +0800

    update

--- lib/introduction_myself.dart
+++ lib/introduction_myself.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class IntroductionMyself extends StatelessWidget {
   const IntroductionMyself({super.key});
@@ -13,7 +14,7 @@ class IntroductionMyself extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 ClipOval(
                   child: Image.network('https://avatars.githubusercontent.com/u/143181701?v=4',width: 150,),
@@ -26,7 +27,7 @@ class IntroductionMyself extends StatelessWidget {
                     children: [
                       Text("關於我",style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Color.fromRGBO(103, 80, 164, 1),),),
                       Divider(),
-                      Text("我是douobb，是一位就讀於交大資工的flutter菜鳥，正在學習如何使用flutter建立網站以及APP，希望這一個網站對正在挑選機殼的你有幫助(❁´◡`❁)",style: TextStyle(fontSize: 18, height: 1.5),),
+                      Text("我是douobb，是一位就讀於交大資工的flutter菜鳥，正在學習如何使用flutter建立網站以及APP，本網頁算是我做的第一個正式項目，希望對正在挑選機殼的你有幫助",style: TextStyle(fontSize: 18, height: 1.5),),
                     ],
                   ),
                 )
@@ -35,9 +36,100 @@ class IntroductionMyself extends StatelessWidget {
             const SizedBox(height: 30,),
             const Text("關於本網站",style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Color.fromRGBO(103, 80, 164, 1),),),
             const Divider(),
-            //整理成流程圖
-            const Text("本網站的資料是利用Python結合GitHub Action定期在原價屋網站上爬取的，爬取資料之後在Python整理，"
-                "整理完成後上傳至Firebase的資料庫保存，網站建立則是使用flutter以及GitHub Pages來實現。",style: TextStyle(fontSize: 18, height: 1.5),),
+            const Text("本網站建立的過程主要可以分成以下幾個階段：",style: TextStyle(fontSize: 18, height: 1.5),),
+            const SizedBox(height: 15,),
+            Container(
+              width: 300,
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+              decoration: BoxDecoration(
+                color: const Color.fromRGBO(230, 223, 239, 1),
+                borderRadius: const BorderRadius.all(Radius.circular(4.0)),
+                border: Border.all(width: 1, color: const Color.fromRGBO(103, 80, 164, 1)),
+              ),
+              child: const Column(
+                children: [
+                  Text("資料蒐集",style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Color.fromRGBO(103, 80, 164, 1),),),
+                  Divider(indent: 30, endIndent: 30,),
+                  Text("Python + GitHub Action 定期在原價屋網站上爬取",style: TextStyle(fontSize: 16, height: 1.5),),
+                ],
+              )
+            ),
+            const SizedBox(
+              height: 40,
+              width: 300,
+              child: Center(
+                child: Icon(Icons.arrow_downward),
+              )
+            ),
+            Container(
+                width: 300,
+                padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                decoration: BoxDecoration(
+                  color: const Color.fromRGBO(230, 223, 239, 1),
+                  borderRadius: const BorderRadius.all(Radius.circular(4.0)),
+                  border: Border.all(width: 1, color: const Color.fromRGBO(103, 80, 164, 1)),
+                ),
+                child: const Column(
+                  children: [
+                    Text("資料儲存",style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Color.fromRGBO(103, 80, 164, 1),),),
+                    Divider(indent: 30, endIndent: 30,),
+                    Text("Python中整理為數個List，之後上傳到Firebase的資料庫保存",style: TextStyle(fontSize: 16, height: 1.5),),
+                  ],
+                )
+            ),
+            const SizedBox(
+                height: 40,
+                width: 300,
+                child: Center(
+                  child: Icon(Icons.arrow_downward),
+                )
+            ),
+            Container(
+                width: 300,
+                padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                decoration: BoxDecoration(
+                  color: const Color.fromRGBO(230, 223, 239, 1),
+                  borderRadius: const BorderRadius.all(Radius.circular(4.0)),
+                  border: Border.all(width: 1, color: const Color.fromRGBO(103, 80, 164, 1)),
+                ),
+                child: const Column(
+                  children: [
+                    Text("網站建立",style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Color.fromRGBO(103, 80, 164, 1),),),
+                    Divider(indent: 30, endIndent: 30,),
+                    Text("使用flutter + GitHub Pages部署到Github上",style: TextStyle(fontSize: 16, height: 1.5),),
+                  ],
+                )
+            ),
+            const SizedBox(height: 30,),
+            const Text("並且在這過程中我在Github上建立了兩個專案，如果對於程式碼有興趣的可以到以下的專案內看看：",style: TextStyle(fontSize: 18, height: 1.5),),
+            const SizedBox(height: 15,),
+            ElevatedButton(
+              style: ButtonStyle(
+                splashFactory: NoSplash.splashFactory,
+                elevation: MaterialStateProperty.all(0),
+                backgroundColor: MaterialStateProperty.all(const Color.fromRGBO(103, 80, 164, 1)),
+              ),
+              onPressed: (){
+                launchUrl(Uri.parse('https://github.com/douobb/pc_case_data'), mode:LaunchMode.externalApplication);
+              },
+              child: const Text("pc_case_data",style: TextStyle(fontSize: 16, color: Colors.white),),
+            ),
+            const SizedBox(height: 10,),
+            const Text("定期爬取資料，使用Python語言編寫",style: TextStyle(fontSize: 18, height: 1.5),),
+            const SizedBox(height: 15,),
+            ElevatedButton(
+              style: ButtonStyle(
+                splashFactory: NoSplash.splashFactory,
+                elevation: MaterialStateProperty.all(0),
+                backgroundColor: MaterialStateProperty.all(const Color.fromRGBO(103, 80, 164, 1)),
+              ),
+              onPressed: (){
+                launchUrl(Uri.parse('https://github.com/douobb/pc_case_filter'), mode:LaunchMode.externalApplication);
+              },
+              child: const Text("pc_case_filter",style: TextStyle(fontSize: 16, color: Colors.white),),
+            ),
+            const SizedBox(height: 10,),
+            const Text("本網站的主體，使用Flutter(Dart)語言編寫",style: TextStyle(fontSize: 18, height: 1.5),),
           ],
         ),
       ),
